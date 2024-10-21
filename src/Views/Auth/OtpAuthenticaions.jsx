2ab458414fdb1f8d1b222d5d9287f146efee37b8
Zubairkhawar-11 <Zubair@saeedantechpvt.com>
2024-10-21 18:51:49 +0500

    otp

--- src/Views/Auth/OtpAuthenticaions.jsx
+++ src/Views/Auth/OtpAuthenticaions.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Grid, Box, Typography, TextField, Button } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux"; // Import useDispatch
+import { useDispatch, useSelector } from "react-redux";
 import signinImage from "../../assets/picture/signin.png";
 import { resendOtp, verifyOtp } from "../../store/reducers/action";
 import { useNavigate } from "react-router-dom";
@@ -9,14 +9,23 @@ const OtpAuthentications = () => {
   const [otp, setOtp] = useState(Array(4).fill(""));
   const [errorMessage, setErrorMessage] = useState("");
   const dispatch = useDispatch();
-  const data = useSelector((stat) => stat?.admin?.user)
-  // console.log(data, 'ujooooooooooo')
-  const navigate = useNavigate()
+  const data = useSelector((state) => state?.admin?.user);
+  const navigate = useNavigate();
+  const inputRefs = useRef([]); // Create a ref array for the input fields
 
   const handleOtpChange = (index, value) => {
     const newOtp = [...otp];
     newOtp[index] = value;
     setOtp(newOtp);
+
+    // Move to the next input if the current one is filled
+    if (value && index < 3) {
+      inputRefs.current[index + 1].focus();
+    }
+    // Move to the previous input if the current one is empty and not the first input
+    else if (!value && index > 0) {
+      inputRefs.current[index - 1].focus();
+    }
   };
 
   const handleSubmit = () => {
@@ -28,15 +37,8 @@ const OtpAuthentications = () => {
 
     dispatch(verifyOtp(otpData))
       .then((response) => {
-        if (response.payload.success) {
-          console.log("OTP verified successfully!");
-
-          // Navigate to next page or show success message
-          setErrorMessage(response?.data?.payload?.message || "OTP verification failed.");
-        } else {
-          setErrorMessage(error?.response?.data?.message || "OTP verification failed.");
-        }
-        console.log(response, 'jjjjjjjjj')
+        console.log("OTP verified successfully!");
+        navigate('/watch-videos', { replace: true }); // Replace the current entry in the history stack
       })
       .catch((error) => {
         setErrorMessage(error?.response?.data?.message || "OTP verification failed.");
@@ -46,64 +48,35 @@ const OtpAuthentications = () => {
   const handleResendOtp = () => {
     const resendData = {
       email: data.email,
-      // otp: otpCode,
     };
 
     dispatch(resendOtp(resendData))
       .then((response) => {
         if (response.payload.success) {
-          console.log("OTP resend successfully!");
-          console.log(response, 'jjjjjjjjj')
-          navigate('/watch-videos')
-          // Navigate to next page or show success message
-          setErrorMessage(response?.data?.payload?.message || "OTP verification failed.");
+          console.log("OTP resent successfully!");
+          setErrorMessage(response.payload.message || "OTP resent successfully.");
         } else {
-          setErrorMessage(error?.response?.data?.message || "OTP verification failed.");
+          setErrorMessage(response.payload.message || "Failed to resend OTP.");
         }
       })
       .catch((error) => {
-        setErrorMessage(error?.response?.data?.message || "OTP verification failed.");
+        setErrorMessage(error?.response?.data?.message || "Failed to resend OTP.");
       });
   };
 
-
-
   return (
-    <Grid
-      container
-      spacing={2}
-      sx={{
-        alignItems: "center",
-        marginTop: "5px",
-        padding: { xs: "10px", sm: "20px" },
-      }}
-    >
-      <Grid
-        item
-        xs={12}
-        md={6}
-        sx={{ display: "flex", justifyContent: "center" }}
-      >
+    <Grid container spacing={2} sx={{ alignItems: "center", marginTop: "5px", padding: { xs: "10px", sm: "20px" } }}>
+      <Grid item xs={12} md={6} sx={{ display: "flex", justifyContent: "center" }}>
         <Box sx={{ width: { xs: "100%", sm: "80%" }, textAlign: "left" }}>
           <Box sx={{ mb: 4 }}>
             <img src={"/Logo.jpg"} alt="Logo" style={{ maxWidth: "200px" }} />
           </Box>
 
-          <Typography
-            variant="h4"
-            sx={{
-              mb: 2,
-              fontWeight: 700,
-              fontSize: { xs: "1.8rem", md: "2.125rem" },
-            }}
-          >
+          <Typography variant="h4" sx={{ mb: 2, fontWeight: 700, fontSize: { xs: "1.8rem", md: "2.125rem" } }}>
             OTP Authentication
           </Typography>
 
-          <Typography
-            variant="body1"
-            sx={{ mb: 3, fontSize: { xs: "0.9rem", sm: "1rem" } }}
-          >
+          <Typography variant="body1" sx={{ mb: 3, fontSize: { xs: "0.9rem", sm: "1rem" } }}>
             Please enter the four-digit verification code we have sent to{" "}
             <Typography sx={{ color: "#0294D3", display: "inline-block" }}>
               {data?.email}
@@ -116,39 +89,25 @@ const OtpAuthentications = () => {
                 key={index}
                 variant="outlined"
                 placeholder="-"
-                inputProps={{
-                  maxLength: 1,
-                  style: {
-                    textAlign: "center",
-                    fontSize: "1.5rem",
-                  },
-                }}
+                inputProps={{ maxLength: 1, style: { textAlign: "center", fontSize: "1.5rem" } }}
                 value={otp[index]}
                 onChange={(e) => handleOtpChange(index, e.target.value)}
-                sx={{
-                  width: { xs: "40px", sm: "50px", md: "90px" },
-                  marginRight: index !== 3 ? { xs: "8px", sm: "12px" } : "0",
+                onKeyDown={(e) => {
+                  if (e.key === 'Backspace' && !otp[index] && index > 0) {
+                    inputRefs.current[index - 1].focus();
+                  }
                 }}
+                inputRef={(ref) => inputRefs.current[index] = ref}
+                sx={{ width: { xs: "40px", sm: "50px", md: "90px" }, marginRight: index !== 3 ? { xs: "8px", sm: "12px" } : "0" }}
               />
             ))}
           </Box>
 
-          {errorMessage && (
-            <Typography variant="body1" color="error" sx={{ mb: 2 }}>
-              {errorMessage}
-            </Typography>
-          )}
+          {errorMessage && <Typography variant="body1" color="error" sx={{ mb: 2 }}>{errorMessage}</Typography>}
 
-          <Typography
-            variant="body1"
-            sx={{ mb: 4, fontSize: { xs: "0.9rem", sm: "1rem" } }}
-          >
+          <Typography variant="body1" sx={{ mb: 4, fontSize: { xs: "0.9rem", sm: "1rem" } }}>
             Don’t receive the OTP?{" "}
-            <Typography
-              onClick={handleResendOtp}
-              component="span"
-              sx={{ color: "#0294D3", cursor: "pointer" }}
-            >
+            <Typography onClick={handleResendOtp} component="span" sx={{ color: "#0294D3", cursor: "pointer" }}>
               Resend OTP
             </Typography>
           </Typography>
@@ -156,34 +115,16 @@ const OtpAuthentications = () => {
           <Button
             size="large"
             onClick={handleSubmit}
-            sx={{
-              width: "100%",
-              background: "#0294D3",
-              color: "white",
-              padding: "10px 0",
-              fontSize: { xs: "0.9rem", sm: "1rem" },
-              "&:hover": {
-                background: "#027bb3",
-              },
-            }}
+            sx={{ width: "100%", background: "#0294D3", color: "white", padding: "10px 0", fontSize: { xs: "0.9rem", sm: "1rem" }, "&:hover": { background: "#027bb3" } }}
           >
             Confirm
           </Button>
         </Box>
       </Grid>
 
-      <Grid
-        item
-        xs={12}
-        md={6}
-        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
-      >
+      <Grid item xs={12} md={6} sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
         <Box sx={{ width: { xs: "90%", md: "100%" }, height: "100%" }}>
-          <img
-            src={signinImage}
-            alt="Sign in"
-            style={{ height: "470px", width: "100%" }}
-          />
+          <img src={signinImage} alt="Sign in" style={{ height: "470px", width: "100%" }} />
         </Box>
       </Grid>
     </Grid>
